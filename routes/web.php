744015ae7e841476244850b9b3a3ab893b1a6c9f
SnorlaxSPI <aflinsjr@icloud.com>
2025-06-16 19:47:04 -0300

    Route Parameters With Constraints

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,22 @@ Route::view('/view', 'home', ['myName' => 'Alessandro']);
 
 Route::get('/valor/{value}', [MainController::class, 'mostrarValor']);
 Route::get('/user/{user_id}/post/{post_id}', [MainController::class, 'mostrarPosts']);
+
+// -----------------------------------------
+// ROUTE PARAMETERS WITH CONSTRINTS
+// -----------------------------------------
+
+Route::get('/exp1/{value}', function ($value) {
+  echo "O valor é: $value";
+})->where('value', '[0-9]+'); // Aceita apenas números
+
+Route::get('/exp2/{value}', function ($value) {
+  echo "O valor é: $value";
+})->where('value', '[a-zA-Z]+'); // Aceita apenas letras
+
+Route::get('/exp3/{value1}/{value2}', function ($value) {
+  echo "O valor é: $value";
+})->where([
+  'value1' => '[0-9]+',
+  'value2' => '[a-zA-Z0-9]+'
+]);
