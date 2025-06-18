f146074fef98fba59a2bd2cb4e8206c7549fd006
SnorlaxSPI <aflinsjr@icloud.com>
2025-06-18 11:45:17 -0300

    Controllers e subpastas

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\SingleActionController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/init', [MainController::class, 'initMethod'])->name('init');
-Route::get('/view', [MainController::class, 'viewPage'])->name('view');
-
-//route para controller single action
-Route::get('/single', SingleActionController::class)->name('single');
-
-// route para controller de recursos
-//Route::resource('users', UserController::class);
-
-Route::resources([
-  'users' => UserController::class,
-  'clients' => ClientsController::class,
-  'products' => ProductsController::class
-]);
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
