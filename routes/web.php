51fe03c85c1e7ffc9ab69cfcb34eef3cca37a87c
SnorlaxSPI <aflinsjr@icloud.com>
2025-06-18 11:36:30 -0300

    Single action controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/init', [MainController::class, 'initMethod'])->name('init');
 Route::get('/view', [MainController::class, 'viewPage'])->name('view');
+
+//route para controller single action
+Route::get('/single', SingleActionController::class)->name('single');
+
+// route para controller de recursos
+//Route::resource('users', UserController::class);
+
+Route::resources([
+  'users' => UserController::class,
+  'clients' => ClientsController::class,
+  'products' => ProductsController::class
+]);
