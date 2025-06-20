f724249e89f332905eb22cd5f257b0ed1e1b7856
SnorlaxSPI <aflinsjr@icloud.com>
2025-06-20 16:38:57 -0300

    para que serve o controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,4 +2,12 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller {}
+abstract class Controller
+{
+  // remover os espaços em branco no início e no final de uma string
+  // converter a string para uppercase (maiúsculas)
+  public function cleanUpperCaseString($string): string
+  {
+    return strtoupper(trim($string));
+  }
+}
